1adb2f6116348361ec6d243e1c41dd5e0a8ab8de
Robin Delbaere <hello@rdelbaere.fr>
2025-02-03 11:35:00 +0100

    Setup backend for push

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import cors from "cors";
+import webpush from "web-push";
+import dotenv from "dotenv";
 import { Server } from "socket.io";
 
+dotenv.config();
+
+webpush.setVapidDetails(
+    'mailto:hello@rdelbaere.fr',
+    process.env.VAPID_PUBLIC_KEY,
+    process.env.VAPID_PRIVATE_KEY
+);
+
 const messages = [];
+const subscriptions = [];
 
 const app = express();
 app.use(cors());
@@ -10,6 +21,17 @@ app.use(express.json());
 
 app.get("/messages", (req, res) => {
    return res.json(messages);
+});
+
+app.get("/push/key", (req, res) => {
+   return res.json({
+      pubkey: process.env.VAPID_PUBLIC_KEY,
+   });
+});
+
+app.post("/push/sub", (req, res) => {
+    subscriptions.push(req.body);
+    return res.json({});
 });
 
 app.listen(4000);
